9f2eb8bc3e31e2f93e0b434bdda27e6587bebee8
ab <abelperdan@gmail.com>
2024-05-19 16:07:46 +0200

    Module 8, checkout form

--- obj/Release/net8.0/PieShop.AssemblyInfo.cs
+++ obj/Release/net8.0/PieShop.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PieShop")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1c50113191c07abee6e0ce00201c076ec762e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PieShop")]
