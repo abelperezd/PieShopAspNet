528b07e889b20d2c1d9dbfdaa776f49ab9ff0050
ab <abelperdan@gmail.com>
2024-05-20 19:42:06 +0200

    Unit testing

--- obj/Debug/net8.0/PieShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/PieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2eb8bc3e31e2f93e0b434bdda27e6587bebee8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50213081dccc039f58e24946923d51a44cdd49a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
